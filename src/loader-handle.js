ae9b92ed7168c048d0c52d7ba347879e648917de
Pasi Harju <pasi.harju@livion.fi>
2015-05-14 10:28:03 +0300

    change code to support CommonJS only

--- src/loader-handle.js
+++ src/loader-handle.js
@@ -1,56 +1,41 @@
 /*!
- * LvLoadingSpinner
+ * loader-handle
  * MIT licensed
  *
  * Copyright (C) 2015 Pasi Harju, www.livion.fi
  */
-/* jshint node:true, browser:true */
 
-(function( root, factory ) {
- 'use strict';
+'use strict';
+var duration = 300; // in ms
+var emitter = new (require('events').EventEmitter)(),		
+	elem = document.getElementById('loading-spinner'),	
+	stopTimer = null, 
+	startTimer = null;
 
- var animDuration = 300; // in ms
-  /* CommonJS */
-  if (typeof exports === 'object')  { module.exports = factory(animDuration); }
-
-  /* AMD module */
-  else if (typeof define === 'function' && define.amd) { define(factory); }
-
-  /* Browser global */
-  else { root.loaderHandle = factory(); }
-
-
-  // init dom element
-var s = document.getElementById('loading-spinner').style;
+	  // init dom element
+var s = elem.style;
 	s.opacity = "1";
 	s.display = "block";
 	s.webkitTransitionProperty = "opacity";
 	s.transitionProperty = "opacity";
-	s.webkitTransitionDuration = animDuration + "ms";		
-	s.transitionDuration = animDuration + "ms";		
+	s.webkitTransitionDuration = duration + "ms";		
+	s.transitionDuration = duration + "ms";			
 
-}
-(this, function(duration) {
-	'use strict';
-	var EventEmitter = require('events').EventEmitter,
-		eventEmitter = new EventEmitter(),
-		elem = document.getElementById('loading-spinner'),
-		stopTimer = null, 
-		startTimer = null;	
-
-	var start = function (cb){		
-		eventEmitter.once('started',function(){							
+// Public API
+module.exports = {
+	start:function (cb){		
+		emitter.once('started',function(){							
 			if(typeof cb === 'function') { cb(); }
 		});
 		if(stopTimer){
 			clearTimeout(stopTimer);
 			stopTimer = null;		
-			eventEmitter.emit('stopped');
+			emitter.emit('stopped');
 		}
 		if(!startTimer){
 			startTimer= setTimeout(function(){
 				startTimer = null;			
-				eventEmitter.emit('started');
+				emitter.emit('started');
 			},duration);
 		}
 		setTimeout(function(){
@@ -58,37 +43,27 @@ var s = document.getElementById('loading-spinner').style;
 			setTimeout(function(){
 				elem.style.opacity = "1";		
 			},0);			
-		},0);
-		
-	};
-
-	var stop = function (cb){		
-		eventEmitter.once('stopped',function(){							
+		},0);		
+	},
+	stop:function (cb){		
+		emitter.once('stopped',function(){							
 			elem.style.display = "none";
 			if(typeof cb === 'function') { cb(); }
 		});
 		if(startTimer){
 			clearTimeout(startTimer);
 			startTimer = null;		
-			eventEmitter.emit('started');
+			emitter.emit('started');
 		}
 		if(!stopTimer){
 			stopTimer= setTimeout(function(){
 				stopTimer = null;			
-				eventEmitter.emit('stopped');
+				emitter.emit('stopped');
 			},duration);
 		}
 			
 		setTimeout(function(){
 			elem.style.opacity = "0";				
 		},0);
-
-	};
-
-
-	// Public API
-	return {
-		stop: stop,
-		start: start		
-	};
-}));
+	}
+}	
